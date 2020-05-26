2c0d7d3f27f801ad6e028d05d550ebb161d4d01a
Nate Jones <nate@endot.org>
2020-05-25 20:42:25 -0700

    Extract out pure printing logic

--- test/empath_test.clj
+++ test/empath_test.clj
@@ -1,7 +1,41 @@
 (ns empath-test
   (:require
-    [clojure.test :refer :all]
-    [empath :refer :all]
+    [clojure.edn :as edn]
+    [clojure.string :as string]
+    [clojure.test :refer [deftest is testing]]
+
+    [cheshire.core :as json]
+
+    [empath :refer [prepare-output munge-path analyze]]
+    ))
+
+(def table-result
+  (str "|----------------+--------+------+-------+-----------|\n"
+       "|     Element    | Exists |  Dir |  File | Can Write |\n"
+       "|----------------+--------+------+-------+-----------|\n"
+       "| /usr/bin       | true   | true | false | false     |\n"
+       "| /usr/local/bin | true   | true | false | false     |\n"
+       "|----------------+--------+------+-------+-----------|"))
+
+(deftest print
+  (testing "prepare"
+    (is (= table-result
+           (prepare-output {} (analyze "/usr/bin:/usr/local/bin"))))
+    (is (= table-result
+           (prepare-output {:table true} (analyze "/usr/bin:/usr/local/bin"))))
+    (is (= [{"element" "/usr/bin" "exists" true "dir" true "file" false "can-write" false}
+            {"element" "/usr/local/bin" "exists" true "dir" true "file" false "can-write" false}]
+           (-> (prepare-output {:json true} (analyze "/usr/bin:/usr/local/bin"))
+               (string/split #"\n")
+               (->> (map json/parse-string)))))
+    (is (= [{:element "/usr/bin" :exists true :dir true :file false :can-write false}
+            {:element "/usr/local/bin" :exists true :dir true :file false :can-write false}]
+           (-> (prepare-output {:edn true} (analyze "/usr/bin:/usr/local/bin"))
+               (string/split #"\n")
+               (->> (map edn/read-string)))))
+    (is (= (str "/usr/bin\n"
+                "/usr/local/bin")
+           (prepare-output {:plain true} (analyze "/usr/bin:/usr/local/bin"))))
     ))
 
 (deftest edit
